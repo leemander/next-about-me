da869af2ab86ec687d77a4b0a2c1d6456d7894f2
Lee Mander <lee.mander@gmail.com>
2023-11-20 11:55:45 +0000

    Add header and footer components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
-
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <Header />
+      <main></main>
+      <Footer />
+    </>
+  );
 }
