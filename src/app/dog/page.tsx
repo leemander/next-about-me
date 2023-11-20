c2eafed3838c4813b2a73d10c71c6deb48aab378
Lee Mander <lee.mander@gmail.com>
2023-11-20 15:08:59 +0000

    Add content to pages

--- src/app/dog/page.tsx
+++ src/app/dog/page.tsx
@@ -1,8 +1,25 @@
+import List from "@/components/List";
+
 export default function Page() {
   return (
     <main className="bg-orange-200 text-red-900 py-5">
       <div className="container mx-auto h-full">
         <h2 className="text-2xl font-bold">My Dawg</h2>
+        <img
+          className="w-80 h-80 object-cover rounded-full border-green-500 border-8 mx-auto my-4"
+          src="/grace_bday.jpg"
+          alt="Grace"
+        />
+        <p>
+          I have a pet dog called Grace. Grace is a German Shorthaired Pointer
+          (GSP) and she is 3 and a half years old.
+        </p>
+        <h3 className="text-xl font-semibold my-4">
+          Grace's favourite things:
+        </h3>
+        <List
+          array={["Tennis balls", "Cheese", "Watching for cats", "Belly rubs"]}
+        />
       </div>
     </main>
   );
--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,8 +1,19 @@
+import List from "@/components/List";
+
 export default function Page() {
   return (
     <main className="bg-orange-200 text-red-900 py-5">
       <div className="container mx-auto h-full">
         <h2 className="text-2xl font-bold">My Favourite Video Games</h2>
+        <List
+          array={[
+            "Super Mario Galaxy",
+            "The Legend of Zelda: Breath of the Wild",
+            "Rocket League",
+            "Half Life 2",
+            "Rock Band",
+          ]}
+        />
       </div>
     </main>
   );
