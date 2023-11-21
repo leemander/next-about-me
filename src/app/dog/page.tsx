f144adcb342e3f2de6d8c42de456377113d72aa1
Lee Mander <lee.mander@gmail.com>
2023-11-21 13:52:30 +0000

    Add image filter functionality

--- src/app/dog/page.tsx
+++ src/app/dog/page.tsx
@@ -1,15 +1,60 @@
 import List from "@/components/List";
+import Link from "next/link";
 
-export default function Page() {
+type filterQuery = { filter: string };
+
+export default function Page({ searchParams }: { searchParams: filterQuery }) {
+  const filter = searchParams.filter;
+
   return (
     <main className="bg-orange-200 text-red-900 py-5">
       <div className="container mx-auto h-full">
         <h2 className="text-2xl font-bold">My Dawg</h2>
-        <img
-          className="w-80 h-80 object-cover rounded-full border-green-500 border-8 mx-auto my-4"
-          src="/grace_bday.jpg"
-          alt="Grace"
-        />
+        <div className="mx-auto w-1/2 grid my-4">
+          <div className="bg-orange-400 w-full rounded-t-xl">
+            <img
+              className={`${filter} w-80 h-80 object-cover rounded-full my-4 mx-auto`}
+              src="/grace_bday.jpg"
+              alt="Grace"
+            />
+          </div>
+          <div>
+            <ul className=" bg-green-700 flex items-center text-center h-full px-8 py-4 w-full gap-2 w-full rounded-b-xl">
+              <li className="w-full">
+                <Link
+                  className="p-4 bg-orange-400 w-full h-full block"
+                  href={`/dog?filter=invert`}
+                >
+                  Invert
+                </Link>
+              </li>
+              <li className="w-full">
+                <Link
+                  className="p-4 bg-orange-400 w-full h-full block"
+                  href={`/dog?filter=sepia`}
+                >
+                  Sepia
+                </Link>
+              </li>
+              <li className="w-full">
+                <Link
+                  className="p-4 bg-orange-400 w-full h-full block"
+                  href={`/dog?filter=grayscale`}
+                >
+                  Grayscale
+                </Link>
+              </li>
+              <li className="w-full">
+                <Link
+                  className="p-4 bg-orange-400 w-full h-full block"
+                  href={`/dog`}
+                >
+                  Remove Filter
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
         <p>
           I have a pet dog called Grace. Grace is a German Shorthaired Pointer
           (GSP) and she is 3 and a half years old.
