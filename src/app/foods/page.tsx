038a4902f9109ada2595e8c778bab0b3e70d8942
Lee Mander <lee.mander@gmail.com>
2023-11-22 11:31:14 +0000

    Add food gallery

--- src/app/foods/page.tsx
+++ src/app/foods/page.tsx
@@ -12,6 +12,24 @@ export default function Page() {
       <div className="container mx-auto h-full">
         <h2 className="text-2xl font-bold">My Favourite Food</h2>
         <List array={["Pizza", "Curry", "Noodles"]} />
+        <h3 className="text-2xl font-semibold my-4">Gallery</h3>
+        <div className="flex gap-8">
+          <img
+            src="https://images.unsplash.com/photo-1605478371310-a9f1e96b4ff4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Pepperoni pizza"
+            className="food__img"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1585937421612-70a008356fbe?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Indian curry"
+            className="food__img"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1577859714523-5f0b6c98ece7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Beef noodles"
+            className="food__img"
+          />
+        </div>
       </div>
     </main>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="bg-orange-200 text-red-900">
       <div className="container mx-auto grid grid-cols-2 items-center h-full">
         <img
-          className="h-80 w-80 rounded-full border-green-500 border-8 border-solid mx-auto my-4"
+          className="h-100 w-100 rounded-full border-green-500 border-8 border-solid mx-auto my-4"
           src="https://avatars.githubusercontent.com/u/76167331?v=4"
           alt="Lee Mander"
         />
